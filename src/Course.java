32db0f7c8fec5ed20e236d32a7f22de17c1f5794
tnagar72 <tpnagar@wisc.edu>
2022-03-27 15:56:34 -0500

    Worked on the Course Class and the testCourse method i the ExamSchedulerTester class. Tester passes

--- src/Course.java
+++ src/Course.java
@@ -1,2 +1,30 @@
 public class Course {
+  // Defining data fields
+  private String name; // Name of the Course
+  private int numStudents; // Number of Students in the course
+
+  //Implementing Constructor
+  public Course(String name, int numStudents) throws IllegalArgumentException {
+    //Making sure the integer provided is not negative
+    if (numStudents < 0) {
+      // integer is negative
+      throw new IllegalArgumentException("Invalid Input: The numStudents integer passed into "
+          + "the Course constructor at Object Creation is negative");
+    }
+    // Initializing name
+    this.name = name;
+    // Initializing numStudents
+    this.numStudents = numStudents;
+  } // course_Constructor ends
+
+  //Implementing the getName() and the getNumStudents() methods
+
+  public String getName(){
+    return this.name;
+  } // getName() ends
+
+  public int getNumStudents(){
+    return this.numStudents;
+  } // getNumStudents() ends
+
 }
--- src/ExamSchedulerTester.java
+++ src/ExamSchedulerTester.java
@@ -1,2 +1,106 @@
 public class ExamSchedulerTester {
+
+  public static void main(String[] args) {
+    //Running tests
+    System.out.println("testCourse(): " + testCourse());
+  }
+
+  // Implementing the testCourse() method
+  public static boolean testCourse() {
+    return /*testCourseValidInputTester() &&*/ testCourseInvalidInputTester();
+  } // testCourse ends
+
+  private static boolean testCourseValidInputTester() {
+    //Defining local variables
+    String testName; // String parameter for testing testCourse
+    int testNumStudents; // int parameter for testng testCourse
+    Course testObj; //Object to test course with
+
+    // Define test case
+    //Test 1: getName() correctly returns String value of name which was initialized using
+    // the constructor
+    // Assigning values to variables
+    testName = "CS300";
+    testNumStudents = 1000;
+    //Testing
+    try {
+      // Creating Course Object with test Values
+      testObj = new Course(testName, testNumStudents);
+
+      //Checking test condition if the getName methods returns name of course correctly
+      if (!(testObj.getName().equals(testName))) {
+        System.out.println("Problem Detected: Your getName() method does not return the String "
+            + "variable that was passed to it at Object Creation");
+        return false;
+      }
+    } catch (Exception e) {
+      // the object creation should not throw an exception. Returning false if it does
+      e.printStackTrace();
+      System.out.println("Problem Detected: Your Course class's getName method threw an "
+          + "exception when it shouldn't have");
+      return false;
+    }
+
+    // Test 2: getNumStudents() correctly returns the int value of the numStudents variable
+    // which was initialized/set using the class' parameterized constructor
+    // Assigning values to variables
+    testName = "CS300";
+    testNumStudents = 1000;
+    //Testing
+    try {
+      // Creating Course Object with test Values
+      testObj = new Course(testName, testNumStudents);
+
+      //Checking test condition if the getName methods returns name of course correctly
+      if (testObj.getNumStudents() != testNumStudents) {
+        System.out.println("Problem Detected: Your getNumStudents() method does not return the "
+            + "int variable that was passed to it at Object Creation"
+            + " ");
+        return false;
+      }
+    } catch (Exception e) {
+      // the object creation should not throw an exception. Returning false if it does
+      e.printStackTrace();
+      System.out.println("Your Course class's getNumStudents method threw an exception when it "
+          + "shouldn't have");
+      return false;
+    }
+    // All the above tests passed so returning true
+    return true;
+  }
+
+  private static boolean testCourseInvalidInputTester() {
+    //Defining local variables
+    String testName; // String parameter for testing testCourse
+    int testNumStudents; // int parameter for testng testCourse
+    Course testObj; //Object to test course with
+
+    // Define test case (test case 1 and 2 in ValidInputTester)
+    // Test 3: Constructor throws an IllegalArgumentException (when int value < 0)
+    // Assigning appropriate values to variables
+    testName = "CS 300";
+    testNumStudents = -1; //negative value
+    //Testing
+    try {
+      // Course Object Creation
+      testObj = new Course(testName, testNumStudents);
+      // Expected: Constructor should throw an IllegalArgument Exception since value of
+      // numStudents is negative
+      System.out.println("Problem Detected: Your class Course's constructor does not throw an "
+          + "exception when it should");
+      return false; // since this line should not be reached
+    } catch (IllegalArgumentException e) {
+      if (e.getMessage() == null || e.getMessage().length() == 0) {
+        System.out.println("Problem Detected: Exception message does not have a description");
+        return false;
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+      System.out.println("Problem Detected: Your class Course's constructor throws an Exception"
+          + " other than an IllegalArgument Exception when passed a negative int value");
+      return false;
+    }
+    // All the above tests passed so returning true
+    return true;
+  }
 }
