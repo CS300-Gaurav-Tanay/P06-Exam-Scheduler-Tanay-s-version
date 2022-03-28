55857df39d4526928da1f3af1defb5803ecac47e
tnagar72 <tpnagar@wisc.edu>
2022-03-28 03:04:47 -0500

    Made basic boiler plate structure for the Room class with default return values

--- src/Room.java
+++ src/Room.java
@@ -1,2 +1,20 @@
 public class Room {
-}
+  //Defining data fields
+
+  //Constructor
+  public Room(String location, int capacity) {
+
+  } // constructor ends
+
+  public String getLocation() {
+    return "default";
+  } // getLocation() ends
+
+  public int getCapacity() {
+    return 0;
+  } // getCapacity() ends
+
+  public Room reduceCapacity(int numToReduce) {
+    return null;
+  } // reduceCapacity() ends
+} // Room ends
