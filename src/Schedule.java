1361d7298ca1fd10d92f4d06466b766169f54771
tnagar72 <tpnagar@wisc.edu>
2022-03-28 23:07:05 -0500

    Completed all methods of Schedule except everything after assignCourse(). Have not written test cases yet.

--- src/Schedule.java
+++ src/Schedule.java
@@ -1,2 +1,119 @@
+import java.util.Arrays;
+
 public class Schedule {
+  // Defining data fields
+  private Room[] rooms; // an array of the Room objects available for exams
+  private Course[] courses; // an array of the Course objects which require exam rooms
+  private int[] assignments; // an array where the integer at index N is the index of the room
+  // that course[N] has been assigned to
+
+  // Implementing the constructors
+
+  public Schedule(Room[] rooms, Course[] courses) {
+    // Initializing the rooms and the courses array
+    this.rooms = rooms;
+    this.courses = courses;
+    // Initializing assignment array to be the length of the courses array and the making each
+    // element -1
+    this.assignments = new int[this.courses.length];
+    Arrays.fill(this.assignments, -1);
+  }
+
+  private Schedule(Room[] rooms, Course[] courses, int[] assignments) {
+    // Initializing the rooms and the courses array
+    this.rooms = rooms;
+    this.courses = courses;
+    // Initializing assignments to the provided value
+    this.assignments = assignments;
+  }
+
+  // Implementing methods
+  public int getNumRooms() {
+    return this.rooms.length;
+    //todo: confirm implementation with professor
+  }
+
+  public Room getRoom(int index) throws IndexOutOfBoundsException {
+    if (index < 0 || index >= rooms.length) {
+      // todo: check with gaurav
+      // index is invalid
+      throw new IndexOutOfBoundsException("Invalid Input: the integer parameter passed in to "
+          + "the getRoom() method is either negative (< 0) more than/equal to the size of the "
+          + "rooms array");
+    }
+
+    return this.rooms[index];
+  }
+
+  public int getNumCourses() {
+    return this.courses.length;
+  }
+
+  public Course getCourse(int index) throws IndexOutOfBoundsException {
+    if (index < 0 || index >= courses.length) {
+      // todo: check with gaurav
+      // index is invalid
+      throw new IndexOutOfBoundsException("Invalid Input: the integer parameter is either "
+          + "negative (< 0) more than/equal to the size of the courses array");
+    }
+
+    return this.courses[index];
+  }
+
+  public boolean isAssigned(int index) {
+    // Since the course and the assignments are a parallel array
+    if (assignments[index] != -1) {
+      return true;
+    } else {
+      return false;
+    }
+
+    // todo: check with gaurav and the professor
+  }
+
+  public Room getAssignment(int index)
+      throws IllegalArgumentException, IndexOutOfBoundsException {
+    // todo: check with gaurav
+    // Declaring local variables
+    int roomIndex;
+    Course tempObj;
+
+    // Checking if the course has/has not been assigned to a room. If not, throwing an
+    // IllegalArgumentException
+
+    if (!(this.isAssigned(index))) {
+      throw new IllegalArgumentException(
+          "Invalid Input: The course at index " + index + " has " + "not been assigned yet");
+    }
+
+    // Using the getCourse() method to assign the tmpObj object with the course at index "index"
+    // This checks if the index is valid or not and throws an indexOutOfBoundsException if not
+    // valid
+
+    tempObj = this.getCourse(index);
+
+    //Now that we have checked that both the index is valid and the course is assigned with a
+    // room,returning the room assigned with the course at index "index"
+
+    roomIndex = assignments[index];
+    return this.getRoom(roomIndex);
+  }
+
+  public boolean isComplete() {
+    // Declaring local variables
+    int i; // loop control variable
+
+    // Running a for loop to iterate through index of each course
+    for (i = 0; i < this.getNumCourses(); i++) {
+      if (!(this.isAssigned(i))) {
+        return false;
+      }
+    }
+
+    //Since the whole for loop has been iterated through, all courses have been assigned and so
+    // retuning true
+    return true;
+  }
+
+
 }
